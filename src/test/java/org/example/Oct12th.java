92d9035d7d6322330d740048e5e0594b0fc84206
raj <yadrajshinde1@gmail.com>
2024-10-12 15:11:32 +0530

    2nd commit

--- src/test/java/org/example/Oct12th.java
+++ src/test/java/org/example/Oct12th.java
@@ -1,2 +1,13 @@
-package org.example;public class Oct12th {
+package org.example;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Oct12th {
+
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.close();
+    }
 }
